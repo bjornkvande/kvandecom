c816b35be67985a781cce755ff34ea9ddcd77b7b
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-01-06 19:10:06 +0100

    small refactor

--- components/BlogEntry.tsx
+++ components/BlogEntry.tsx
@@ -1,3 +1,5 @@
+import { Image } from "./Image.tsx";
+
 export function BlogEntry(
   props: {
     title: string;
@@ -12,15 +14,14 @@ export function BlogEntry(
       <h3 className="text-3xl text-gray-700 leading-10">{props.title}</h3>
 
       <a href={`/blog/${props.url}`}>
-        <img
-          src={`https://res.cloudinary.com/trailguide-as/image/upload/v1728468082/${props.image}`}
+        <Image
           className="w-56 ml-8 mt-8 rounded md:float-right"
+          src={props.image}
         />
       </a>
 
       <div className="text-gray-600 italic flex items-center text-sm sm:text-base my-6">
         <img
-          alt="author profile"
           className="shadow w-8 h-8 rounded-full mr-3"
           src={"https://res.cloudinary.com/trailguide-as/image/upload/c_limit,w_400/v1/bjorn/bjorn-portrait-2"}
         />
