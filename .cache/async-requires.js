a32dbdc207593e159a8a1df83179e28862c210c5
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-01-30 11:54:06 +0100

    removing generated files

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---src-pages-blog-automatisert-testing-mdx": () => import("./../../../src/pages/blog/automatisert_testing.mdx" /* webpackChunkName: "component---src-pages-blog-automatisert-testing-mdx" */),
-  "component---src-pages-blog-codeaholic-mdx": () => import("./../../../src/pages/blog/codeaholic.mdx" /* webpackChunkName: "component---src-pages-blog-codeaholic-mdx" */),
-  "component---src-pages-blog-gold-or-led-mdx": () => import("./../../../src/pages/blog/gold_or_led.mdx" /* webpackChunkName: "component---src-pages-blog-gold-or-led-mdx" */),
-  "component---src-pages-blog-invester-i-produktet-mdx": () => import("./../../../src/pages/blog/invester-i-produktet.mdx" /* webpackChunkName: "component---src-pages-blog-invester-i-produktet-mdx" */),
-  "component---src-pages-index-mdx": () => import("./../../../src/pages/index.mdx" /* webpackChunkName: "component---src-pages-index-mdx" */)
-}
-
--- .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/e8a8ece3e6fc4dd8ad108cb6bb3ac659.js
+++
@@ -1,6 +0,0 @@
-import { Image } from "../../../../src/components";
-import * as React from 'react';
-export default {
-  Image,
-  React
-};
